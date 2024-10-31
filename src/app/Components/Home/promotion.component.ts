b841278eee819f0ad56a5a318aaf38a53d14f01d
menna <mennaallah.elsayed23@gmail.com>
2024-10-31 15:48:54 +0300

    Update-home

--- src/app/Components/Home/promotion.component.ts
+++ src/app/Components/Home/promotion.component.ts
@@ -5,11 +5,12 @@ import { BrandsComponent } from '../Homepage_components/brands/brands.component'
 import { FeaturesComponent } from '../Homepage_components/features/features.component';
 import { LastViewComponent } from '../Homepage_components/last-view/last-view.component';
 import { TopOffersComponent } from '../Homepage_components/top-offers/top-offers.component';
+import { PromoCodesComponent } from '../Homepage_components/promo-codes/promo-codes.component';
 
 @Component({
   selector: 'app-promotion',
   standalone: true,
-  imports: [BannerComponent,BrandsComponent,FeaturesComponent,ProductListComponent,LastViewComponent,TopOffersComponent],
+  imports: [BannerComponent,BrandsComponent,FeaturesComponent,ProductListComponent,LastViewComponent,TopOffersComponent,PromoCodesComponent],
   templateUrl: './promotion.component.html',
   styleUrl: './promotion.component.css'
 })
---
+++ src/app/Components/Homepage_components/promo-codes/promo-codes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promo-codes',
+  standalone: true,
+  imports: [],
+  templateUrl: './promo-codes.component.html',
+  styleUrl: './promo-codes.component.css'
+})
+export class PromoCodesComponent {
+
+}
