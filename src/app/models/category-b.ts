6c097e015912fc4f11238bea22c0680e90736fff
KhaledAshrafMohammed <109352509+KhaledAshrafMohammed@users.noreply.github.com>
2024-11-01 19:06:02 +0200

    reset categorymodel

--- src/app/models/category-b.ts
+++ src/app/models/category-b.ts
@@ -2,8 +2,6 @@ import { ProductCategoryB } from './product-category-b';
 import { CategoryTranslationB } from './category-translation-b';
 
 export interface CategoryB {
-  categoryId: number;
-  category: any;
   id: number;
   imageUrl?: string;
   productCategories: ProductCategoryB[];
