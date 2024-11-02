3c76794ead808c5c0f9ce5b2f4d7932ddd4f5f08
KhaledAshrafMohammed <109352509+KhaledAshrafMohammed@users.noreply.github.com>
2024-11-02 19:01:40 +0200

    fix some errors

--- src/app/Components/products-by-brand/products-by-brand.component.ts
+++ src/app/Components/products-by-brand/products-by-brand.component.ts
@@ -3,15 +3,18 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AllproductsService } from '../../service/product/allproducts.service';
 import { ProductB } from '../../models/product-b';
 import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LocalizationService } from '../../service/localiztionService/localization.service';
 
 @Component({
   selector: 'app-products-by-brand',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './products-by-brand.component.html',
   styleUrl: './products-by-brand.component.css'
 })
 export class ProductsByBrandComponent implements OnInit {
+  isArabic!: boolean;
 
 
   brandName: string = '';
@@ -23,7 +26,10 @@ export class ProductsByBrandComponent implements OnInit {
 
     private route: ActivatedRoute,
     private productservice: AllproductsService,
-  ){}
+    private translate: LocalizationService
+  ) {
+    this.translate.IsArabic.subscribe((ar) => (this.isArabic = ar));
+  }
 
   ngOnInit(): void {
     this.brandName = this.route.snapshot.paramMap.get('brandName') || '';
@@ -33,11 +39,11 @@ export class ProductsByBrandComponent implements OnInit {
   fetchProductsByBrand(brandName: string): void{
     this.productservice.getallproducts().subscribe((response: any) => {
       const allProducts: ProductB[] = response.entity || []; // الوصول إلى المنتجات داخل entity
-  
-      this.products = allProducts.filter(product => 
+
+      this.products = allProducts.filter(product =>
         product.translations.some(translation => translation.brandName?.toLowerCase() === brandName.toLocaleLowerCase())
       );
-  
+
       console.log("Filtered products:", this.products); // التحقق من المنتجات بعد التصفية
     },
     error => {
@@ -48,7 +54,7 @@ export class ProductsByBrandComponent implements OnInit {
 
   openProductDetails(data: any) {
     const productId = data?.id || data.product?.id;
-  
+
     if (productId) {
       console.log('Navigating to product details with ID:', productId);
       this.router.navigate(['/details', productId]);
@@ -57,9 +63,12 @@ export class ProductsByBrandComponent implements OnInit {
     }
   }
 
-  
+
 add(event: Event) {
   event.stopPropagation(); // Prevent click event from bubbling up
   this.item.emit(this.products);
+}
+getFormattedPrice(price: number): string {
+  return this.isArabic ? `${price} ج.م` : `$${price}`;
 }
 }
