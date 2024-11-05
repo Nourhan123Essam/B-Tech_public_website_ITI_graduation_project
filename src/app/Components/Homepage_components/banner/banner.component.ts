2a4d7684442ccca248cb3db65ef2dc090752ec68
KhaledAshrafMohammed <109352509+KhaledAshrafMohammed@users.noreply.github.com>
2024-11-05 22:22:56 +0200

    localization update in home page and slider and products page and item

--- src/app/Components/Homepage_components/banner/banner.component.ts
+++ src/app/Components/Homepage_components/banner/banner.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { Route, Router } from '@angular/router';
-
+import { LocalizationService } from '../../../service/localiztionService/localization.service';
 interface Image {
+  url_en: string;
+  url_ar: string;
   url: string;
   createdAt: Date;
 }
@@ -17,18 +19,29 @@ interface Image {
 })
 export class BannerComponent implements OnInit {
   images: Image[] = [];
+  isArabic!: boolean;
 
   constructor(private http: HttpClient,
-    private router: Router
-  ) {}
+    private router: Router,
+    private localizationService: LocalizationService
+  ) {
+    this.localizationService.IsArabic.subscribe((ar) => {
+      this.isArabic = ar;
+    });
+  }
 
   ngOnInit(): void {
     this.loadImages();
   }
 
+
   loadImages() {
     this.http.get<Image[]>('http://localhost:3000/Images').subscribe((data) => {
       this.images = data
+        .map((image) => ({
+          ...image,
+          url: this.isArabic ? image.url_ar : image.url_en
+        }))
         .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
         .slice(0, 5);
     });
