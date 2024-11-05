a72e914375c61d226ec48c52ce3be3d39532b885
KhaledAshrafMohammed <109352509+KhaledAshrafMohammed@users.noreply.github.com>
2024-11-05 11:26:41 +0200

    localize in home page

--- src/app/Components/Homepage_components/features/features.component.ts
+++ src/app/Components/Homepage_components/features/features.component.ts
@@ -1,23 +1,27 @@
-import { Component , OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LocalizationService } from '../../../service/localiztionService/localization.service';
 
 @Component({
   selector: 'app-features',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './features.component.html',
-  styleUrl: './features.component.css'
+  styleUrl: './features.component.css',
 })
-export class FeaturesComponent  implements OnInit {
+export class FeaturesComponent implements OnInit {
+  isArabic!: boolean;
+
   // categories = [
   //   { name: 'Mobiles', imageUrl: 'assets/images/mobile.jpg' },
   //   { name: 'Appliances', imageUrl: 'assets/images/appliances.jpg' },
   //   { name: 'Electronics', imageUrl: 'assets/images/electronics.jpg' }
   // ];
 
-  constructor() {}
+  constructor(private translate: LocalizationService) {
+    this.translate.IsArabic.subscribe((ar) => (this.isArabic = ar));
+  }
 
   ngOnInit(): void {}
 }
-
-
-
