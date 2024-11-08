4ea81ca3be317b08fc391480fd4725df55595a13
KhaledAshrafMohammed <109352509+KhaledAshrafMohammed@users.noreply.github.com>
2024-11-09 01:21:07 +0200

    my account localization

--- src/app/Components/my-account/my-account.component.ts
+++ src/app/Components/my-account/my-account.component.ts
@@ -2,26 +2,30 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { AuthService } from '../../service/Identity/auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LocalizationService } from '../../service/localiztionService/localization.service';
 
 @Component({
   selector: 'app-my-account',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './my-account.component.html',
-  styleUrl: './my-account.component.css'
+  styleUrl: './my-account.component.css',
 })
-export class MyAccountComponent  implements OnInit {
+export class MyAccountComponent implements OnInit {
   userName: string = '';
   phoneNumber: string = '';
   isUserLoggedIn: boolean = false;
-
+  isArabic!: boolean;
 
   constructor(
-     private authService: AuthService,
-     private router: Router,
-     private cdr: ChangeDetectorRef, // إضافة ChangeDetectorRef هنا
-
-  ) {}
+    private authService: AuthService,
+    private router: Router,
+    private cdr: ChangeDetectorRef, // إضافة ChangeDetectorRef هنا
+    private translate: LocalizationService
+  ) {
+    this.translate.IsArabic.subscribe((ar) => (this.isArabic = ar));
+  }
 
   ngOnInit(): void {
     this.userName = this.authService.getUserNameFromToken() || ''; // إذا كان null أو undefined يتم تعيينه كـ ''
@@ -32,6 +36,5 @@ export class MyAccountComponent  implements OnInit {
     this.isUserLoggedIn = false;
     this.router.navigate(['/']);
     this.cdr.detectChanges(); // تحديث العرض بعد تسجيل الخروج
-
   }
 }
